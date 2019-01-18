8f61d5aec32d87052d94ea0b5c7b9b77814bcb5d
CallumWilliams <callum.a.williams@gmail.com>
2019-01-17 22:59:08 -0500

    rebuilt autocomplete and place picker method

--- app/src/main/java/project/remindme/SelectLocation.java
+++ app/src/main/java/project/remindme/SelectLocation.java
@@ -1,174 +1,189 @@
 package project.remindme;
 
 import android.Manifest;
-import android.content.Intent;
+import android.content.Context;
 import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
 import android.location.Location;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.AutoCompleteTextView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
-import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.PendingResult;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.places.AutocompletePrediction;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.PlaceBuffer;
 import com.google.android.gms.location.places.Places;
-import com.google.android.gms.location.places.ui.PlacePicker;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
-import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-public class SelectLocation extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.OnConnectionFailedListener {
+public class SelectLocation extends FragmentActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, OnMapReadyCallback {
 
     @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {}
-
-    private GoogleMap mMap;
-    private Reminder r;
-
-    private final static int PLACE_PICKER_REQUEST = 1;
-    private boolean mLocationPermissionsGranted = false;
-    private int DEFAULT_ZOOM = 15;
-    private static LatLngBounds LATLNGBOUNDS = new LatLngBounds(new LatLng(-40, -168), new LatLng(71, 136));
-
-    PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
-    private FusedLocationProviderClient fusedLocationProviderClient;
-    private GoogleApiClient googleApiClient;
-    private AutoCompleteTextView search;
-    private PlaceAutoCompleteAdapter autoCompleteAdapter;
-
-    protected boolean hasLocationPermissions() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return false;
-        }
-        return true;
+    public void onConnected(@Nullable Bundle bundle) {
+        placeArrayAdapter.setGoogleApiClient(googleApiClient);
     }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        placeArrayAdapter.setGoogleApiClient(null);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this, connectionResult.getErrorMessage(), Toast.LENGTH_SHORT).show();
+    }
+
+    private GoogleMap gMap;
+    private Reminder r;
+    private String location_name;
+
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private Boolean locationPermissionsGranted;
+    private AutoCompleteTextView autoCompleteTextView;
+    private GoogleApiClient googleApiClient;
+    private PlaceArrayAdapter placeArrayAdapter;
+
+    private static final float DEFAULT_ZOOM = 15;
+    private static final LatLngBounds BOUNDS = new LatLngBounds(
+            new LatLng(37.398160, -122.180831), new LatLng(37.430610, -121.972090));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_location);
-        search = findViewById(R.id.input_search);
-        mLocationPermissionsGranted = hasLocationPermissions();
+
         r = (Reminder) getIntent().getSerializableExtra("Reminder");
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        googleApiClient = new GoogleApiClient.Builder(SelectLocation.this)
+                .addApi(Places.GEO_DATA_API)
+                .enableAutoManage(this, 0, this)
+                .addConnectionCallbacks(this)
+                .build();
+
+        locationPermissionsGranted = getLocationPermissions();
+
+        autoCompleteTextView = findViewById(R.id.input_search);
+        autoCompleteTextView.setThreshold(3);
+
+        autoCompleteTextView.setOnItemClickListener(autoCompleteClickListener);
+
+        placeArrayAdapter = new PlaceArrayAdapter(this, android.R.layout.simple_list_item_1, BOUNDS, null);
+        autoCompleteTextView.setAdapter(placeArrayAdapter);
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
-    private void moveCamera(LatLng latlng, float zoom) {
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, zoom));
+    private Boolean getLocationPermissions() {
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        }
+        return false;
     }
 
-    private void getDeviceLocation() {
+    private void getDeviceLocation(){
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
         try {
-            if (mLocationPermissionsGranted) {
-                final Task loc = fusedLocationProviderClient.getLastLocation();
-                ((Task) loc).addOnCompleteListener(new OnCompleteListener() {
+            if (locationPermissionsGranted) {
+
+                final Task location = fusedLocationProviderClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
                     @Override
                     public void onComplete(@NonNull Task task) {
-                        if (task.isSuccessful()) {
-                            Location curr = (Location) task.getResult();
-                            moveCamera(new LatLng(curr.getLatitude(), curr.getLongitude()), DEFAULT_ZOOM);
+                        if(task.isSuccessful()){
+                            Location currentLocation = (Location) task.getResult();
+
+                            moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()),
+                                    DEFAULT_ZOOM,
+                                    "My Location");
+
                         } else {
-                            Toast.makeText(SelectLocation.this, "Unable to get current location", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(SelectLocation.this, "unable to get current location", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
             }
-        } catch (SecurityException e) {
-            e.getStackTrace();
-        } catch (Exception e) {
-            e.getStackTrace();
+        } catch (SecurityException e){
+            Log.e("Security Exception", e.getMessage() );
         }
     }
 
-    private void geoLocate() {
-        String to_search = search.getText().toString();
+    private void moveCamera(LatLng latLng, float zoom, String title) {
+        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
 
-        Geocoder geo = new Geocoder(SelectLocation.this);
-        List<Address> list = new ArrayList<>();
-        try {
-            list = geo.getFromLocationName(to_search, 1);
-        } catch (IOException e) {
-            Log.d("Geocoder error: ", e.getMessage());
+        if(!title.equals("My Location")){
+            MarkerOptions options = new MarkerOptions().position(latLng).title(title);
+            gMap.addMarker(options);
         }
 
-        if (list.size() > 0) {
-            // found location - go there
-            Address a = list.get(0);
-            moveCamera(new LatLng(a.getLatitude(), a.getLongitude()), DEFAULT_ZOOM);
-        }
+        hideSoftKeyboard();
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    private void hideSoftKeyboard() {
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+    }
+
+    private AdapterView.OnItemClickListener autoCompleteClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            final PlaceArrayAdapter.PlaceAutocomplete item = placeArrayAdapter.getItem(position);
+            final String placeId = String.valueOf(item.placeId);
+            PendingResult<PlaceBuffer> placeResult = Places.GeoDataApi.getPlaceById(googleApiClient, placeId);
+            placeResult.setResultCallback(updatePlaceDetailsCallback);
+        }
+    };
+
+    private ResultCallback<PlaceBuffer> updatePlaceDetailsCallback = new ResultCallback<PlaceBuffer>() {
+        @Override
+        public void onResult(PlaceBuffer places) {
+            if (!places.getStatus().isSuccess()) {
+                return;
+            }
+            // Selecting the first object buffer.
+            final Place place = places.get(0);
+            CharSequence attributions = places.getAttributions();
+            location_name = place.getName().toString();
+            moveCamera(place.getLatLng(), DEFAULT_ZOOM, place.getName().toString());
+        }
+    };
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        Toast.makeText(this, "MAP READY", Toast.LENGTH_SHORT).show();
+        gMap = googleMap;
 
-        if (mLocationPermissionsGranted) {
+        if (locationPermissionsGranted) {
             getDeviceLocation();
 
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 return;
             }
-            mMap.setMyLocationEnabled(true);
-            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+            gMap.setMyLocationEnabled(true);
+            gMap.getUiSettings().setMyLocationButtonEnabled(false);
         }
-        googleApiClient = new GoogleApiClient.Builder(this).addApi(Places.GEO_DATA_API).addApi(Places.PLACE_DETECTION_API).enableAutoManage(this, this).build();
-        autoCompleteAdapter = new PlaceAutoCompleteAdapter(this, googleApiClient, LATLNGBOUNDS, null);
-
-        search.setAdapter(autoCompleteAdapter);
-
-        search.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_DONE || actionId == EditorInfo.IME_ACTION_SEARCH || event.getAction() == KeyEvent.ACTION_DOWN || event.getAction() == KeyEvent.KEYCODE_ENTER) {
-                    geoLocate();
-                }
-                return false;
-            }
-        });
 
     }
 
