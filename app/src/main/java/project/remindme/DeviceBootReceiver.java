23b0308fc819a3f63202ffac8284024f84dbc707
CallumWilliams <callum.a.williams@gmail.com>
2019-02-04 13:39:09 -0500

    fixed device boot receiver

--- app/src/main/java/project/remindme/DeviceBootReceiver.java
+++ app/src/main/java/project/remindme/DeviceBootReceiver.java
@@ -1,20 +1,18 @@
 package project.remindme;
 
-import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
 
 public class DeviceBootReceiver extends BroadcastReceiver {
 
     @Override
-    public void onReceive(Context c, Intent arg) {
-        Intent i = new Intent(c, LocationCheckService.class);
-        c.startService(i);
-        Log.i("DEVICEBOOTRECEIVER", "ACTIVE");
+    public void onReceive(Context context, Intent intent) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            Toast.makeText(context, "Boot completed", Toast.LENGTH_SHORT).show();
+        }
     }
 
 }
